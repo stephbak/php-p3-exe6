6504ff64ee5169eab5c6ea5c0efb92c3ca8d0277
stephbak <stephane.bakum@sfr.fr>
2019-01-07 10:21:33 +0100

    p3 exe6

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<link rel="stylesheet" href="style.css" />
+<head>
+  <meta charset="utf-8" />
+  <title>php partie3</title>
+</head>
+<body>
+  <?php for($number = 20; $number >= 0; $number--){ ?>
+    <p>C'est presque bon!</p>
+  <?php } ?>
+</body>
+</html>
